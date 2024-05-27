1d03266c1b61dd810e455f82f63f80d3d4f54faf
ryanneilsalas <141245597+ryanneilsalas@users.noreply.github.com>
2024-05-27 16:49:52 +0800

    update for  my projects

---
+++ quiz/note.py
@@ -0,0 +1,38 @@
+# from turtle import Turtle, Screen
+# squirtle = Turtle()
+# squirtle.shape("turtle")
+# my_screen = Screen()
+# squirtle.color("blue3")
+# squirtle.forward(100)
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokémon Name", ['Pikachu', "Squirtle", "Charmander"])
+# table.add_column("Type", ['Electric', "Water", "Fire"])
+# print(table)
+# class User:
+#     def __init__(self, user_id, username):
+#         self.id = user_id
+#         self.username = username
+#         self.followers = 0
+#         self.following = 0
+#
+#     def follow(self, user):
+#         user.followers += 1
+#         self.following += 1
+#
+#
+# user_1 = User("001", "Ryan")
+# user_2 = User("002", "Angela")
+#
+# user_1.follow(user_2)
+# print(user_1.followers)
+# print(user_1.following)
+# print(user_2.followers)
+# print(user_2.following)
+
+
+
+
+
