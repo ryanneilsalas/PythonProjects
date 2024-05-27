3d0d01cfd3b083c9eb98ce0bc090634669b9f93f
ryanneilsalas <141245597+ryanneilsalas@users.noreply.github.com>
2024-05-27 16:50:08 +0800

    update for  my projects

--- CoffeMachine.py
+++ CoffeMachine.py
@@ -82,3 +82,27 @@ while is_on:
         print(f"Here is your {pick_coffee}. Enjoy!")
     else:
         print("Sorry, We dont have that coffe")
+
+# from menu import Menu
+# from coffee_maker import CoffeeMaker
+# from money_machine import MoneyMachine
+#
+# money_machine = MoneyMachine()
+# coffee_maker = CoffeeMaker()
+# menu = Menu()
+#
+# is_on = True
+#
+# while is_on:
+#     options = menu.get_items()
+#     choice = input(f"What would you like? ({options}): ")
+#     if choice == "off":
+#         is_on = False
+#     elif choice == "report":
+#         coffee_maker.report()
+#         money_machine.report()
+#     else:
+#         drink = menu.find_drink(choice)
+#
+#         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+#             coffee_maker.make_coffee(drink)
